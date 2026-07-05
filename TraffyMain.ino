8c4d850973e484350e9091020db810551baf3c01
Vampzro <marxmoeckel@gmail.com>
2026-07-06 00:21:04 +0200

    Reset of the code base
    Changed the code base to the old version due to an issue.

--- TraffyMain.ino
+++ TraffyMain.ino
@@ -19,57 +19,156 @@
  *   11  - ear servo signal
  */
 
-#include "src/MicSensor.h"
-#include "src/RGBLed.h"
-#include "src/TailServo.h"
-#include "src/EarServo.h"
-#include "src/Buzzer.h"
+#include <Servo.h>
 
-//Thresholds 
-const int THRESHOLD_LOUD = 7;
-const int THRESHOLD_MID  = 4;
+//microphone (MAX4466)
+const int micPin = A0;
 
-//Component instances
-MicSensor mic(A0);
-RGBLed    led(3, 5, 6);
-TailServo tail(9);
-EarServo  ear(11);
-Buzzer    buzzer(10);
+//LEDs
+const int redPin = 3;
+const int greenPin = 5;
+const int bluePin = 6;
 
+//servo motor (tail)
+const int servoPin = 9;
+Servo tailServo;
 
+//servo motor (ears)
+const int earServoPin = 11;
+Servo earServo;
+
+//passive buzzer
+const int buzzerPin = 10;
+
+//thresholds
+const int blueThreshold = 1;
+const int redThreshold = 20;
+
+//setup
 void setup() {
   Serial.begin(9600);
-  led.begin();
-  tail.begin();
-  ear.begin();
-  buzzer.begin();
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  pinMode(buzzerPin, OUTPUT);
+
+  //tail servo 
+  tailServo.attach(servoPin);
+  tailServo.write(90);
+
+  //ear servo 
+  earServo.attach(earServoPin);
+  earServo.write(30); // Ohren unten
 }
 
 void loop() {
-  int loudness = mic.read();
+
+  int loudness = 0;
+
+  // 5 Messungen mitteln
+  for (int i = 0; i < 5; i++) {
+
+    int signalMax = 0;
+    int signalMin = 1023;
+
+    unsigned long start = millis();
+
+    while (millis() - start < 20) {
+      int sample = analogRead(micPin);
+
+      if (sample > signalMax)
+        signalMax = sample;
+
+      if (sample < signalMin)
+        signalMin = sample;
+    }
+
+    loudness += (signalMax - signalMin);
+  }
+
+  loudness /= 5;
+
   Serial.println(loudness);
 
-  if (loudness >= THRESHOLD_LOUD) {
-    //LOUD: alarmed 
-    led.setColor(255, 0, 0);
-    tail.still();
-    ear.up();
-    buzzer.bark();
+  //loud input
+  if (loudness >= redThreshold) {
+
+    setColor(255, 0, 0);
+
+    tailServo.write(90);     // Schwanz still
+    earServo.write(160);     // Ohren oben
+
+    barkSound();
+
     delay(2000);
-
-  } else if (loudness >= THRESHOLD_MID) {
-    //MID: alert
-    led.setColor(0, 0, 255);
-    tail.still();
-    ear.half();
-    delay(3000);
-
-  } else {
-    //QUIET: relaxed 
-    led.setColor(0, 255, 0);
-    ear.down();
-    tail.wag();
-    delay(150);
-    analogRead(A0); 
   }
+
+  //mid input
+  else if (loudness >= blueThreshold) {
+
+    setColor(0, 0, 255);
+
+    tailServo.write(90);
+    earServo.write(75);      // Ohren halb oben
+
+    delay(3000);
+  }
+
+  //quiet input
+  else {
+
+    setColor(0, 255, 0);
+
+    earServo.write(30);      // Ohren unten
+
+    wagTail();
+
+    delay(150);
+    analogRead(micPin);
+  }
+}
+
+//RPG-LED function 
+void setColor(int r, int g, int b) {
+  analogWrite(redPin, r);
+  analogWrite(greenPin, g);
+  analogWrite(bluePin, b);
+}
+
+//tail
+void wagTail() {
+
+  tailServo.write(60);
+  delay(150);
+
+  tailServo.write(120);
+  delay(150);
+
+  tailServo.write(90);
+}
+
+//buzzer 
+void barkSound() {
+
+  for (int i = 0; i < 2; i++) {
+
+    // Tiefes "Wuff"
+    tone(buzzerPin, 200);
+    delay(80);
+
+    // Mittleres Bellen
+    tone(buzzerPin, 600);
+    delay(120);
+
+    // Hoher Peak
+    tone(buzzerPin, 900);
+    delay(100);
+
+    noTone(buzzerPin);
+    delay(60);
+  }
+
+  delay(200);
 }
